5389a209e89e8f5fb62c21491aaa5e7cb69f8201
Eric Davis <edavis@littlestreamsoftware.com>
2008-05-20 15:32:18 -0700

    Setup init information. #1134

--- init.rb
+++ init.rb
@@ -1,13 +1,10 @@
-# Empty redmine plguin
 require 'redmine'
 
-# TODO: Change this to use the name of your plugin
-RAILS_DEFAULT_LOGGER.info 'Starting Empty plugin for RedMine'
+RAILS_DEFAULT_LOGGER.info 'Starting Budget plugin for RedMine'
 
-# TODO: Change the name 
-Redmine::Plugin.register :empty_plugin do
-  name 'Empty plugin'
-  author 'Eric Davis'
-  description 'This is an empty plugin for Redmine that is used to start new plugins'
+Redmine::Plugin.register :budget_plugin do
+  name 'Budget'
+  author 'Eric Davis <edavis@littlestreamsoftware.com>'
+  description 'This is a budget plugin to track deliverables and budgets on a project'
   version '0.0.0'
 end
