ceee1912447ec30c52911d268ca5c17472c94149
Eric Davis <edavis@littlestreamsoftware.com>
2008-05-20 17:03:15 -0700

    Added settings for the system. #1139

--- init.rb
+++ init.rb
@@ -8,6 +8,13 @@ Redmine::Plugin.register :budget_plugin do
   description 'This is a budget plugin to track deliverables and budgets on a project'
   version '0.0.0'
   
+  settings:default => {
+    'budget_nonbillable_overhead' => '',
+    'budget_materials' => '',
+    'budget_profit' => ''
+  }, :partial => 'settings/settings'
+
+  
   project_module :budget_module do
     permission :view_budget, { :deliverables => [:index]}
     permission :manage_budget, { :deliverables => [:new, :edit, :create, :update, :destroy]}
