f8399f1968f5f06c17b7f17d0b12bef2e362c177
Eric Davis <edavis@littlestreamsoftware.com>
2008-05-20 22:27:00 -0700

    Complted the JavaScript subtotals on the new Deliverable form
    * Subtotals will be updated as data is entered
    * Created a helper to setup the fields with observers
    * Added JavaScript functions to calculate the values
    
      #1135

---
+++ app/helpers/deliverables_helper.rb
@@ -0,0 +1,12 @@
+module DeliverablesHelper
+  def field_with_budget_observer_and_totals(form, field)
+    content_tag(:p,
+                form.text_field(field, :size => 7) +
+                content_tag(:span,
+                            0,
+                            :class => "budget-calculation",
+                            :id => 'deliverable_' + field.to_s + '_subtotal'
+                            )
+                ) + observe_field('deliverable_' + field.to_s, :function => "updateSubtotal('deliverable_#{field.to_s}'); updateBudget('deliverable_budget');")
+  end
+end
