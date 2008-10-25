36d9ab3ed88c19c28002f8aecfabc1f2ed937ecf
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-24 22:18:20 -0700

    Removed the monkey patching with Issue.find.  It was required but is no longer needed and keep breaking search.

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -18,28 +18,6 @@ module IssuePatch
   end
   
   module ClassMethods
-
-    # Muck with the find arguements to append an include for deliverables
-    def find(*args)
-      # Options defined
-      if args[1].is_a?(Hash)
-        # Abort the special case of issue.search.  LIKE is used by search
-        if args[1].has_key?(:conditions) && args[1][:conditions].is_a?(Array) && args[1][:conditions][0].match(/LIKE \?/)
-          # skip
-        elsif args[1].has_key?(:include) && !args[1][:include].nil? # include used?
-          # Add our include
-          if args[1][:include].is_a?(Array)
-            args[1][:include] << :deliverable
-          else
-            args[1][:include] = [args[1][:include], :deliverable] # Rewrite a the include as an array
-          end
-        else
-          # Add an include
-          args[1][:include] = [:deliverable]
-        end
-      end
-      super
-    end
     
   end
   
